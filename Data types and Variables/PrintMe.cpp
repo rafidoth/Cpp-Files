067d2ed2e7181b136a4767aeaed84e2ae4e165ac
rafidoth <asdarafi@gmail.com>
2022-01-03 14:42:11 +0600

    r

---
+++ Data types and Variables/PrintMe.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A,B;
+    cin >> A>>B;
+    if(B==1){
+        cout << 2*A +1;
+    }
+    if(B==-1){
+        cout << A*A;
+    }
+
+}
+
